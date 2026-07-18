a848c69ca1d72b6316c493edf99c154c211a0c41
yuriilizan95 <yuriilizan@gmail.com>
2026-07-18 18:49:47 +0200

    make hw 10 js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,43 @@
-console.log('Snackbar');
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+
+  createPromise(delay, state)
+    .then((value) => {
+      iziToast.success({
+        title: 'OK',
+        message: `Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch((value) => {
+      iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${value}ms`,
+        position: 'topRight',
+      });
+    });
+    
+  form.reset(); 
+});
